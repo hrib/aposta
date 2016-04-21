d13d5cf8fbcd7be9029f6f8750cd465da9fbb98c
hugodose <hugo.dose@gmail.com>
2016-04-21 14:18:01 +0100

    Update read_page.php

--- read_page.php
+++ read_page.php
@@ -3,11 +3,14 @@ session_start();
 echo '<br>1<br>';
 require_once 'src/Facebook/autoload.php';
 echo '<br>2<br>';
+
+$app_id = '1011974285544429';
+$app_secret = '9b28ee403af9889f18c3fd6f3b9135c8';
 $fb = new Facebook\Facebook([
-  'app_id' => '1011974285544429',
-  'app_secret' => '9b28ee403af9889f18c3fd6f3b9135c8',
+  'app_id' => $app_id,
+  'app_secret' => $app_secret,
   'default_graph_version' => 'v2.6', // change to 2.5
-  //'default_access_token' => 'CAAOYYpZCPyZB0BAEZCkFzEhIaffQWy01uchvfFom7pSoZBu7Qr6UuVxEizJdb40JXjxkOWtnZB9GzViAvsvPSgdpi9cZBC8o3GUDHJSCfGjthyC5zZCuoZAD3p6G4G1rhztjbEZAD5kRmyArM1XixvcN5cvfo5pG62JzGY1CRYNIPM8XrEZAuE96GBTSk2JzFxOljhwXNZATrAhmEO3cBTM3fpV'
+  //'default_access_token' => ''
 ]);
 echo '<br>3<br>';
 
