a2470d157be6199ebf3056d43c32f4990c6fa9da
hugodose <hugo.dose@gmail.com>
2016-04-21 22:04:20 +0100

    Update read_post.php

--- read_post.php
+++ read_post.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 echo '<br>1<br>';
-require_once 'src/Facebook/autoload.php';
+//require_once 'src/Facebook/autoload.php';
 echo '<br>2<br>';
 
 $app_id = '1011974285544429';
@@ -14,35 +14,7 @@ $fb = new Facebook\Facebook([
 ]);
 echo '<br>3<br>';
 
-$response = $fb->get('204223673035117_364562457001237');
-//var_dump($response->getDecodedBody());
-echo '<br>4<br>';    
-$graphNode = $response->getGraphNode();
-//echo $graphNode['feed'][0]['message'] . '<br><br>';
-foreach ($graphNode['feed'] as $key => $value) {
-  echo '<br>' . $key . ':' . $value['message'] . '<br>';
-  echo '___________________________________________________';
-}
-
-
-//foreach ($graphNode as $key => $value) {
-//  echo $key . '<br>';
-  //echo var_dump($value);
-//  foreach ($value as $key2 => $value2) {
-//    echo $key2 . '<br>';
-//    echo var_dump($value2) . '<br>';
-//    echo '_y_' . $value[0]['message'] . '<br>';
-//      foreach ($value2 as $key3 => $value3) {
-        //echo '__' . $key3 . '<br>';
-        //echo '__' . var_dump($value3) . '<br>';
-//        echo '_z_' . $value2['message'] . '<br>';
-//      }
-//  }
-//  echo '<br>';
-//  echo '<br>';
-//}
-
-$response = $fb->get('/204223673035117_364562457001237');
+$response = $fb->get('204223673035117_364562457001237?fields=comments');
 //var_dump($response->getDecodedBody());
 echo '<br>4<br>';    
 $graphNode = $response->getGraphNode();
