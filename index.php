c2a99aca4ea81a3e1848d6053a30fcbf4c46a45e
hugodose <hugo.dose@gmail.com>
2016-04-21 21:15:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,31 @@ $zica = array('pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$
 echo 'aqui<br><br>';
 echo var_dump($zica);
 echo 'aqui<br><br>';
-$app->register(new Herrera\Pdo\PdoServiceProvider(), $zica);
+
+$dsn = "pgsql:"
+    . "host=ec2-184-73-194-179.compute-1.amazonaws.com;"
+    . "dbname=ul28zxpr39no1rr;"
+    . "user=dj1wcxb3x9fy3x5;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=p28xwd9pjcrzyzp6mf74m99cze";
+
+$db = new PDO($zica);
+
+$query = "SELECT employee_id, last_name, first_name, title "
+    . "FROM employees ORDER BY last_name ASC, first_name ASC";
+$result = $db->query($query);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>" . $row["employee_id"] . "</td>";
+    echo "<td>" . htmlspecialchars($row["last_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["first_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["title"]) . "</td>";
+    echo "</tr>";
+}
+$result->closeCursor();
+
+//$app->register(new Herrera\Pdo\PdoServiceProvider(), $zica);
 echo 'aqui<br><br>';
 
 $app->get('/db/', function() use($app) {
