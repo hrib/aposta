e63c32766fdde7e8d74c28e2377e4443b9ee4a70
hugodose <hugo.dose@gmail.com>
2016-04-24 00:09:48 +0100

    Update clone_post.php

--- clone_post.php
+++ clone_post.php
@@ -19,7 +19,10 @@ foreach ($graphNode['posts'] as $key => $value) {
   echo '<br>' . $key . ':' . $value['link'] . '<br>';
   echo '<br>' . $key . ':' . $value['full_picture'] . '<br>';
   $a = strtotime($value['created_time']->date);
-  echo '<br>' . $key . ':' . $a . '<br>';
+  echo var_dump($value['created_time']->date);
+  echo '<br>'
+  echo var_dump($a);
+  //echo '<br>' . $key . ':' . $a . '<br>';
   $curtime = gmdate(DATE_ISO8601, time());
   echo '<br> diff:' . $curtime - $a . '<br>';
   echo '<br>_______________________________________________<br>';
