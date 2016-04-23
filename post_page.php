a85685fa76f3b008005e904ccfc316441cbe69e4
hugodose <hugo.dose@gmail.com>
2016-04-23 21:34:03 +0100

    Update post_page.php

--- post_page.php
+++ post_page.php
@@ -23,7 +23,7 @@ $fb = new Facebook\Facebook([
 
   // 'link' => 'http://apostagol.herokuapp.com/image.jpg',
 $linkData = [
-  'image' => 'http://apostagol.herokuapp.com/image.jpg',
+  'image' => '@http://apostagol.herokuapp.com/image.jpg',
   'message' => 'oi',
 ];
 
