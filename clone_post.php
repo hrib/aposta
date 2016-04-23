185962f05d639f3cf11a6e715faf436263e5035e
hugodose <hugo.dose@gmail.com>
2016-04-23 22:48:34 +0100

    Update clone_post.php

--- clone_post.php
+++ clone_post.php
@@ -18,7 +18,7 @@ foreach ($graphNode['posts'] as $key => $value) {
   echo '<br>' . $key . ':' . $value['message'] . '<br>';
   echo '<br>' . $key . ':' . $value['link'] . '<br>';
   echo '<br>' . $key . ':' . $value['full_picture'] . '<br>';
-  echo '<br>' . $key . ':' . $value['created_time'] . '<br>';
+  //echo '<br>' . $key . ':' . $value['created_time'] . '<br>';
   $curtime = time();
   echo '<br>' . $curtime . '<br>';
   $z = gmdate(DATE_ISO8601, $curtime);
