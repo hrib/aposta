370887878f9536eda37117bae40309f2b4df1a21
hugodose <hugo.dose@gmail.com>
2016-04-23 15:26:26 +0100

    Update saveimage.php

--- saveimage.php
+++ saveimage.php
@@ -2,7 +2,7 @@
 $imgurl = 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/11988259_1672140403008188_2765317534386607908_n.jpg?oh=ef2d8c9deed78c03d070ae0ac052497e&oe=57AA2100';
 $storedimg = file_put_contents("image.jpg", file_get_contents(imgurl));
 $imgteste = 'http://dedarwin.net/wp-content/uploads/2015/02/HTML.jpg';
-echo '<img src=' . $imgteste . 'alt="Mountain View" style="width:304px;height:228px;">';
+echo '<img src=' . $imgteste . '>';
 
 
 ?>
