58d50beb919acc3f3e9a24e2229d63ede942d655
hugodose <hugo.dose@gmail.com>
2016-04-23 21:03:11 +0100

    Update post_page.php

--- post_page.php
+++ post_page.php
@@ -11,7 +11,7 @@ require_once 'src/Facebook/autoload.php';
 echo '<br>2<br>';
 $app_id = '1011974285544429';
 $app_secret = '9b28ee403af9889f18c3fd6f3b9135c8';
-$page_access_token = 'CAAOYYpZCPyZB0BAH9PGfnT0xZB3Gl6XHvSj5YeTZBnOhnjgZCX3HysliZAXnr2e1uetN07JuZCumdZAAgjHrB1ZCgsqZCeBTuGA2Rp4VN69ElvKlcQH0SPqzilRWZB77XqtorC4D3ZAV7PMibUZAyVZCitsG13UUmS7lyLY6jtrErOug8FkwqpTRlWN2plUFZB0OqxlXbLh9Tbj5qUAgQZDZD';
+$page_access_token = 'CAAOYYpZCPyZB0BAJQJbaTZAeZAACAh2UHWAZC5JFWKWl9ZCAnz9HZBuhvgbSzzEkmiRJSLA76rB130UmFZAVX8oyOoAQJAZB6KzKnZCYb6tVh3El0yexuf92xOMn7u5wTME7dCAlUGrFb7l9BAO4iUitD1wUFgCXKUip3N03NTF4jQQjlrLVUIBhcIp0ZC74HY35Lzs3fDevH46LAZDZD';
 
 $fb = new Facebook\Facebook([
   'app_id' => $app_id,
