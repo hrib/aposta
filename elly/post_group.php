72009f4e7b4d35ba5da627086cfc089ab048d226
hugodose <hugo.dose@gmail.com>
2016-04-24 20:26:24 +0100

    Update post_group.php

--- elly/post_group.php
+++ elly/post_group.php
@@ -5,8 +5,6 @@ session_start();
 
 $imgurl = 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/11988259_1672140403008188_2765317534386607908_n.jpg?oh=ef2d8c9deed78c03d070ae0ac052497e&oe=57AA2100';
 file_put_contents("image.jpg", file_get_contents($imgurl));
-//echo "<img src='image.jpg'>";
-
 
 require_once '../src/Facebook/autoload.php';
 
@@ -14,6 +12,7 @@ $app_id = '874168309359589';
 $app_secret = '5abc1d036bf115bb722115e436ad5f6b';
 $access_token = 'EAAMbDSuNoZBUBAFQlIs4qKKn0VYIPB2eH36bZBSSyt6787TuFSWPHZAcd2RE2KAtpcBsc8oy7cPyO6lOXEsvcvyySsfojeE6o7x8YHGqBKyAZAEeDC1GSDqRdXKVYSvR97rpmvxX9pvYi7xkJapycq84ZC5ZBhVUYZD';
 $albumid = '187737951614546';
+$groupid = '211312725908106';
 
 $fb = new Facebook\Facebook([
   'app_id' => $app_id,
@@ -23,7 +22,6 @@ $fb = new Facebook\Facebook([
 ]);
 
 
-  // 'link' => 'http://apostagol.herokuapp.com/image.jpg',
   //  'image' => 'http://apostagol.herokuapp.com/image.jpg',
 $linkData = [
   'source' => $fb->fileToUpload('image.jpg'),
@@ -32,8 +30,7 @@ $linkData = [
 
 try {
 //    $response = $fb->post('/Theballisonthetable/feed', $linkData, $page_access_token);
-    //$response = $fb->post('/' . $albumid . '/photos', $linkData, $access_token);
-$response = $fb->post('/groups/211312725908106', $linkData, $access_token);
+  $response = $fb->post('/' . $albumid . '/photos', $linkData, $access_token);
 
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
     echo 'Graph returned an error: ' . $e->getMessage();
@@ -42,6 +39,29 @@ $response = $fb->post('/groups/211312725908106', $linkData, $access_token);
     echo 'Facebook SDK returned an error: ' . $e->getMessage();
     exit;
 }
-echo '<br>fim<br>';
+echo '<br>postou no user<br>';
+
+
+
+
+
+
+
+$linkData = [
+  'link' => 'https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/12804656_152766121778396_2102593346909812311_n.jpg?oh=1c9f0cb5f2958ad2c726c0ce55f13d85&oe=57760A9B',
+  'message' => 'teste2',
+];
+
+try {
+  $response = $fb->post('/' . $groupid . '/', $linkData, $access_token);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+
+echo '<br>postou no grupo<br>';
 
 ?>
