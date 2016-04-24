731401129acfeec15d779e8a29583ac918b3fc14
hugodose <hugo.dose@gmail.com>
2016-04-24 22:27:25 +0100

    Update google_images.php

--- elly/google_images.php
+++ elly/google_images.php
@@ -1,25 +1,8 @@
 <?php
 session_start();
 
-function get_url_contents($url) {
-    $crl = curl_init();
-
-    curl_setopt($crl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
-    curl_setopt($crl, CURLOPT_URL, $url);
-    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, 5);
-
-    $ret = curl_exec($crl);
-    curl_close($crl);
-    return $ret;
-}
-
-$json = get_url_contents('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=sausages');
-$data = json_decode($json);
-foreach ($data->responseData->results as $result) {
-    $results[] = array('url' => $result->url, 'alt' => $result->title);
-}
-
-print_r($results);
+$imgurl = 'https://www.google.co.uk/search?q=php+google+images+search&newwindow=1&safe=off&biw=1366&bih=626&source=lnms&tbm=isch&tbs=qdr:y&sa=X&ved=0ahUKEwj95uHemqjMAhXHI8AKHVx2CwE4ChD8BQgHKAE';
+file_put_contents("image.jpg", file_get_contents($imgurl));
+echo "<img src='image.jpg'>";
 
 ?>
