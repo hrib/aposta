53276104ebde24e209e9f76f5e09bac0b47ff5cd
hugodose <hugo.dose@gmail.com>
2016-04-24 02:18:08 +0100

    Update and rename index.php to sql.php

--- sql.php
+++ sql.php
@@ -17,23 +17,20 @@ $dsn = "pgsql:"
 
 $db = new PDO($dsn);
 
-$query = "CREATE TABLE employees ("
-    . "employee_id SERIAL,"
-    . "last_name VARCHAR(30),"
-    . "first_name VARCHAR(30),"
-    . "title VARCHAR(50)"
+$query = "CREATE TABLE dados ("
+    . "id1 VARCHAR(30),"
+    . "id2 VARCHAR(30),"
+    . "id3 VARCHAR(30),"
+    . "id4 VARCHAR(50)"
     . ");";
 $result = $db->query($query);
 echo var_dump($result);
 echo 'aqui<br><br>';
 
-$query = "INSERT INTO employees (last_name, first_name, title) VALUES"
-    . "('Abreu', 'Mark', 'Project Coordinator'),"
-    . "('Nyman', 'Larry', 'Security Engineer'),"
-    . "('Simmons', 'Iris', 'Software Architect'),"
-    . "('Miller', 'Anthony', 'Marketing Manager'),"
-    . "('Leigh', 'Stephen', 'UI Developer'),"
-    . "('Lee', 'Sonia', 'Business Analyst');";
+$query = "INSERT INTO dados (id1, id2, id3, id4) VALUES"
+    . "('brasilreal', '1011974285544429', '9b28ee403af9889f18c3fd6f3b9135c8', 'CAAOYYpZCPyZB0BADOeUcJtI94u9wZAvBPZAzz7be9DqZBBUxM1857DZCAi4WSX4VOhViZAJ5WMs7GoEp9fZC9k4NZBxH7ZBwIAW8JkFqNzbgN7ZB0a0cieHuIBwXfPAQlXFfuYoUZAEdLGqVQ6E3MGwmcpXJt8ycQUPcZCvPznY5RXSQAxhsKsCwZA50IN4ZByoEHZAiCLBHKEVprRZA5lwZDZD'),"
+    . "('theball', '1011974285544429', '9b28ee403af9889f18c3fd6f3b9135c8', 'CAAOYYpZCPyZB0BANxJjtAtka6YZBaxB04IRK9pNYZC0VRLZCREZClhw3gXE0MZBaqZA6e7XAc8ZBZAL5exukNJVThSWVLtx1ZA7In8zc6idDIIlmcHhHnGzm3cbRDsApZBqmyNz7aDTAmlCSGCdzLZBanxZAvLRSiLTfK7jZBRPIYA9uyRNQ5H2mjetwgv0PcBDnZCnn4kHPwjbo7lvfXwZDZD'),"
+    . "('xxx', 'xxxx', 'xxx', 'xx');";
 $result = $db->query($query);
 echo var_dump($result);
 echo 'aqui<br><br>';
