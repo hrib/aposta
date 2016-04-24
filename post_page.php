bc1420c44df6f4017878cded721b51a04d1199b3
hugodose <hugo.dose@gmail.com>
2016-04-24 19:13:54 +0100

    Update post_page.php

--- post_page.php
+++ post_page.php
@@ -7,7 +7,7 @@ file_put_contents("image.jpg", file_get_contents($imgurl));
 //echo "<img src='image.jpg'>";
 
 
-require_once '../src/Facebook/autoload.php';
+require_once '/src/Facebook/autoload.php';
 echo '<br>2<br>';
 $app_id = '1011974285544429';
 $app_secret = '9b28ee403af9889f18c3fd6f3b9135c8';
