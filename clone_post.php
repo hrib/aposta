39233e772c03bd8ae531bf19a35e26f13a4c3f0e
hugodose <hugo.dose@gmail.com>
2016-04-24 01:05:32 +0100

    Update clone_post.php

--- clone_post.php
+++ clone_post.php
@@ -19,11 +19,14 @@ foreach ($graphNode['posts'] as $key => $value) {
   echo '<br>' . $key . ':' . $value['link'] . '<br>';
   echo '<br>' . $key . ':' . $value['full_picture'] . '<br>';
   echo var_dump($value['created_time']);
-  echo var_dump($value['created_time']->date);
+  echo '<br>';
+  //echo var_dump($value['created_time']->date);
+  echo '<br>';
   $created_timeSTR = $value['created_time']->date;
-  echo var_dump($created_timeSTR);
+  //echo var_dump($created_timeSTR);
+  echo '<br>';
   $created_time = strtotime($created_timeSTR);  //unix
-  echo var_dump($created_time);
+  //echo var_dump($created_time);
   echo '<br>str' . $key . ':' . $created_timeSTR . '<br>';
   echo '<br>time' . $key . ':' . $created_time . '<br>';
   $tempo = time();
