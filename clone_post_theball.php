e021fb8d87b8be3fb450631ccfb5f7fc3459e5f0
hugodose <hugo.dose@gmail.com>
2016-04-24 16:22:20 +0100

    Update clone_post_theball.php

--- clone_post_theball.php
+++ clone_post_theball.php
@@ -72,44 +72,4 @@ foreach ($graphNode as $pagina) {
 }
 echo '</table>';
 
-function PostClonezzzzzzz($fb, $myalbumid, $mypageid, $page_access_token, $message, $link, $picture){
-  
-  //echo '<td>POSTANDO</td>';
-  file_put_contents("image.jpg", file_get_contents($picture));
-
-  if (strpos($picture, 'https://scontent') !== false) {
-    //imagem interna, posta como imagem
-    echo '<td>Imagem interna</td>';
-    $target = '/' . $myalbumid . '/photos';
-    $linkData = [
-      'source' => $fb->fileToUpload('image.jpg'),
-      'message' => $message,
-    ];
-  } else {
-    //imagem externa(link) ou sem imagem e link, posta link/nada
-    echo '<td>Link externo/nada</td>';
-    $target = '/' . $mypageid . '/feed';
-    $linkData = [
-      'link' => $link,
-      'message' => $message,
-    ];
-  }
-  //echo '<td></td>';
-  echo '<td>' . $target . '</td>'; //target
-  try {
-      $response = $fb->post($target, $linkData, $page_access_token);
-  } catch(Facebook\Exceptions\FacebookResponseException $e) {
-      echo 'Graph returned an error: ' . $e->getMessage();
-      exit;
-  } catch(Facebook\Exceptions\FacebookSDKException $e) {
-      echo 'Facebook SDK returned an error: ' . $e->getMessage();
-      exit;
-  }
- 
-  set_time_limit(25); 
-  sleep(10);
-  
-}
-
-
 ?>
