addfd89d9591e547e97fafcd0bd310388d2c3d12
hugodose <hugo.dose@gmail.com>
2016-04-25 15:21:15 +0100

    Update BingSearch.php

--- elly/BingSearch.php
+++ elly/BingSearch.php
@@ -1,15 +1,23 @@
 <?php
 session_start();
-
- $Key = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"; // this is my api key
- $request ='https://api.datamarket.azure.com/Bing/Search/v1/Image?Query=%27flamengo%27&Adult=%27Moderate%27';
- $response = file_get_contents($request);
- $jsonobj = json_decode($response);
- echo('<ul ID="resultList">');
- foreach($jsonobj->SearchResponse->Image->Results as $value){
-  echo('<li class="resultlistitem"><a href="' . $value->Url . '">');
-  echo('<img src="' . $value->Thumbnail->Url. '" width="150" height="150"></li>'); 
- }
- echo("</ul>");
-
+$url = 'https://api.datamarket.azure.com/Bing/Search/';
+$accountkey = '4bsI4zHy6e5Tr1IcXdYobAQ4gCujDVZ2fi0nXO7sdRk';
+$searchUrl = $url.'Image?$format=json&Query=';
+$queryItem = 'flamengo';
+$context = stream_context_create(array(
+    'http' => array(
+    'request_fulluri' => true,
+    'header'  => "Authorization: Basic " . base64_encode($accountkey . ":" . $accountkey)
+    )
+));
+$request = $searchUrl . urlencode( '\'' . $queryItem . '\'');
+echo($request);
+$response = file_get_contents($request, 0, $context);
+$jsonobj = json_decode($response);
+echo('<ul ID="resultList">');
+foreach($jsonobj->d->results as $value){                        
+    echo('<li class="resultlistitem"><a href="' . $value->MediaURL . '">');
+    echo('<img src="' . $value->Thumbnail->MediaUrl. '"></li>');
+}
+echo("</ul>");
 ?>
