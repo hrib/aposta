c8f7b002101135a3ba88845a2ecc26b2a1f6060b
hugodose <hugo.dose@gmail.com>
2016-04-25 15:48:29 +0100

    Update bing_to_group.php

--- elly/bing_to_group.php
+++ elly/bing_to_group.php
@@ -7,10 +7,11 @@ $app_secret = '5abc1d036bf115bb722115e436ad5f6b';
 $access_token = 'EAAMbDSuNoZBUBAFQlIs4qKKn0VYIPB2eH36bZBSSyt6787TuFSWPHZAcd2RE2KAtpcBsc8oy7cPyO6lOXEsvcvyySsfojeE6o7x8YHGqBKyAZAEeDC1GSDqRdXKVYSvR97rpmvxX9pvYi7xkJapycq84ZC5ZBhVUYZD';
 $myalbumid = '187737951614546';
 $groupid = array("211312725908106", "643876078994477", "766144473504153", "1108998679119682");
-$pages_to_copy = array('Anonimasgostosasbr','804933709548543','GostosaD');
-$rb = rand(0,2);
-$rc = rand(0,2);
-$pageOriginal = $pages_to_copy[$rb] . ',' . $pages_to_copy[$rc];
+$busca = 'selfie gostosa';
+//$pages_to_copy = array('Anonimasgostosasbr','804933709548543','GostosaD');
+//$rb = rand(0,2);
+//$rc = rand(0,2);
+//$pageOriginal = $pages_to_copy[$rb] . ',' . $pages_to_copy[$rc];
 $fb = new Facebook\Facebook([
   'app_id' => $app_id,
   'app_secret' => $app_secret,
@@ -18,57 +19,51 @@ $fb = new Facebook\Facebook([
   'default_access_token' => $app_id . '|' . $app_secret
 ]);
 echo '<table border="1" style="font-family:arial; font-size:7px;">';
-$response = $fb->get('/?ids='. $pageOriginal .'&fields=name,posts.limit(2){message,link,full_picture,created_time}');
-$graphNode = $response->getGraphNode();
-foreach ($graphNode as $pagina) {
-    foreach ($pagina['posts'] as $key => $value) {
-      echo '<tr>';
-      echo '<td>' . $key . ':' . $pagina['name'] . '</td>';
-      echo '<td>' . $value['message'] . '</td>';
-      echo '<td>' . $value['link'] . '</td>';
-      echo '<td>' . $value['full_picture'] . '</td>';
-      echo '<td>' . var_dump($value['created_time']) . '</td>'; //precisa disso pra funcionar
-      $created_timeSTR = $value['created_time']->date;
-      $created_time = strtotime($created_timeSTR);  //unix
-      echo '<td>' . $key . ':' . $created_timeSTR . '</td>';
-      $tempo = time();
-      $diffunix = $tempo - $created_time;
-      echo '<td> diff tempo:' . $diffunix . '</td>';
-      if($diffunix < (3600 * 24 * 1)){
-        PostCloneUser($fb, $myalbumid, $groupid, $access_token, $value['message'], $value['link'], $value['full_picture']);
-      }else{
-        echo '<td></td>';
-        echo '<td></td>';
-        echo '<td></td>';
-        //echo '<td></td>';
-      }
-      echo '</tr>';
-    }
-}
+echo '<tr>';
+$bingurl = BingSearch($busca);
+PostCloneUser($fb, $myalbumid, $groupid, $access_token, $bingurl);
+echo '</tr>';
 echo '</table>';
-function PostCloneUser($fb, $myalbumid, $groupid, $access_token, $message, $link, $picture){
+
+function BingSearch($busca){
+    $url = 'https://api.datamarket.azure.com/Bing/Search/';
+    $accountkey = '4bsI4zHy6e5Tr1IcXdYobAQ4gCujDVZ2fi0nXO7sdRk';
+    $searchUrl = $url.'Image?$format=json&Query=';
+    $queryItem = $busca;
+    $context = stream_context_create(array(
+        'http' => array(
+        'request_fulluri' => true,
+        'header'  => "Authorization: Basic " . base64_encode($accountkey . ":" . $accountkey)
+        )
+    ));
+    $request = $searchUrl . urlencode( '\'' . $queryItem . '\'');
+    echo($request);
+    $response = file_get_contents($request, 0, $context);
+    $jsonobj = json_decode($response);
+    echo('<ul ID="resultList">');
+    foreach($jsonobj->d->results as $value){                        
+        echo('<li class="resultlistitem"><a href="' . $value->MediaUrl . '">');
+        echo('<img src="' . $value->Thumbnail->MediaUrl. '"></li>');
+    }
+    echo("</ul>");
+    return $value->MediaUrl;
+}
+
+
+
+
+function PostCloneUser($fb, $myalbumid, $groupid, $access_token, $bingurl){
   
   $textos = array("oi! Add?", "add ou follow?", "adiciona ou segue?", "adiciona?", "me segue", "follow me", "quem me add?", "quem me segue?", "oi! Add? :) ", "add ou follow? :) ", "adiciona ou segue? :) ", "adiciona? :) ", "me segue :) ", "follow me :) ", "quem me add? :) ", "quem me segue? :) ", "oi! Add? :* ", "add ou follow? :* ", "adiciona ou segue? :* ", "adiciona? :* ", "me segue :* ", "follow me :* ", "quem me add? :* ", "quem me segue? :* "); 
   $message = $textos[rand(0,sizeof($textos)-1)];
-  file_put_contents("image.jpg", file_get_contents($picture));
-  if ((strpos($picture, 'https://scontent') !== false)  AND (strpos($link, '/videos/') == false)  ) {
-    //imagem interna, posta como imagem
-    echo '<td>Imagem interna</td>';
-    $target = '/' . $myalbumid . '/photos';
-    $linkData = [
-      'source' => $fb->fileToUpload('image.jpg'),
-      'message' => $message,
-    ];
-  } else {
-    //imagem externa(link), video ou sem imagem e link, posta link/nada
-    echo '<td>Link externo/nada</td>';
-    $target = '/me/feed';
-    $linkData = [
-      'link' => $link,
-      'message' => $message,
-    ];
-  }
-  echo '<td>' . $target . '</td>';
+  file_put_contents("image.jpg", file_get_contents($bingurl));
+  $target = '/' . $myalbumid . '/photos';
+  $linkData = [
+    'source' => $fb->fileToUpload('image.jpg'),
+    'message' => $message,
+  ];
+
+  echo '<td>' . $bingurl . '</td>';
   try {
     $response = $fb->post($target, $linkData, $access_token);
   } catch(Facebook\Exceptions\FacebookResponseException $e) {
