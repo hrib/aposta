c80cc4249d21ac2820a48a8ba80fb821e2b04ef9
hugodose <hugo.dose@gmail.com>
2016-05-02 01:34:26 +0100

    Update scrap_bing.php

--- scrap_bing.php
+++ scrap_bing.php
@@ -1,6 +1,6 @@
 <?php
 include 'simple_html_dom.php';
-$html = file_get_html('http://www.bing.com/images/search?q=interior+design&view=detailv2&first=1&selectedindex=2');
+$html = file_get_html('http://www.bing.com/images/search?q=interior+design&view=detailv2&first=1&selectedindex=0&id=21E2D43D44635A4E9D0FAFE011BC90EB39B17CA');
 
 //foreach($html->find('img') as $element)
 //       echo $element->src . '<br>';
